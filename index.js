3c342be1145f68b5ad896c1481052cac3a9fd343
Johanny Grisales <grisales-95@outlook.es>
2021-08-27 17:36:25 -0500

    ejercicio 1 y 2

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 //Imports de los módulos
+// const users = require("./users.json");
+const fs = require("fs");
+const path = require("path");
+const pathUsers = path.resolve("./users.json");
+
 const readFileUsers = () => {
-    //Imprimir en consola el arreglo de usuarios
+  //Imprimir en consola el arreglo de usuarios
+  fs.promises
+    .readFile(pathUsers, "utf8")
+    .then(function (result) {
+      console.log(result);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 };
 
 const writeHelloWorld = () => {
-    //Escribir hello world! en el archivo hello.txt
-
+  const pathFile = path.resolve("./hello.txt");
+  const data = "hello world!";
+  //Escribir hello world! en el archivo hello.txt
+  fs.promises
+    .writeFile(pathFile, data)
+    .then((result) => {
+      console.log("archivo modificado");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const addUser = async (username) => {
-    //Agregar un usuario en la lista users.json
+  fs.promises
+    .appendFile(pathUsers, username)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 //No hace falta ejecutar las funciones
 
 module.exports = {
-    readFileUsers,
-    writeHelloWorld,
-    addUser,
+  readFileUsers,
+  writeHelloWorld,
+  addUser,
 };
